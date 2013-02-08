ee95c42dc9d2423045705a85d1f197264eb64559
Neve <nick.s@rightscale.com>
2013-02-08 03:00:28 +0300

    10.1d1 task integration

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,7 @@ class Cart < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :line_items, dependent: :destroy
 
-  # Add product
+  # Add new product
   # increase quantity if product exists
   # or add new product item line to cart
   def add_product(product_id)
