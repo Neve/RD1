8e74a9a50317a75fe2f86fec5a0f74876204d4ad
Neve <nick.s@rightscale.com>
2013-02-10 17:02:41 +0300

    10.4 activity 1

---
+++ db/migrate/20130210105848_add_price_to_line_items.rb
@@ -0,0 +1,12 @@
+class AddPriceToLineItems < ActiveRecord::Migration
+  def change
+    #add price column
+    #add_column :line_items, :price, :decimal, precision: 8, scale: 2
+    LineItem.reset_column_information
+
+    LineItem.all.each do |li|
+      li.update_attribute :price, li.product.price
+    end
+
+  end
+end
--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -4,4 +4,10 @@ class CartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :carts
+
+  test "test_create_cart" do
+
+  end
+
 end
