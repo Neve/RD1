b6c1ed8b1f237b4f9d85b46db7f8220817a9f269
Neve <nick.s@rightscale.com>
2013-02-17 03:15:55 +0300

    11.7 chapter completed

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,8 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.store .entry > img', 3
+    assert_select '.entry input[type=submit]'
     assert_select '#columns #side a', minimum: 4
     assert_select '#main .entry', 3
     assert_select 'h3', 'Programming Ruby 1.9'
